9f68b8ed96bada4c4dc0405b923ff7bb60e829ff
Ed <edoisa6@gmail.com>
2017-11-17 19:56:39 -0800

    in progress

--- ShellSort.java
+++ ShellSort.java
@@ -5,34 +5,60 @@ public class ShellSort {
 	public void iterative(int[] array) {
 		
 		// find sequence #
-		int h = 1;
+		int seqNum = 1;
 		
-		while (h <= array.length/2)
-			h = 2*h + 1;
+		while (seqNum <= array.length/2)
+			seqNum = (2 * seqNum) + 1;
 		
 		// insertion sort
-		int currentNum = -1; // currently selected number
+		int numAtIndex = -1; // currently selected number
+		int numAtSeqNum = -1; // number at index of sequence #
 		int currentNumIndex = -1; // index of variable currentNum
+		boolean noSwaps = true; // if no more swaps were made, then array must be sorted
+		int tempSeqNum;
 		
-		// select next unsorted num in array and (re)set currentNumIndex
-		for(int i = 0; i < array.length; i++) {
-			currentNum = array[i];
-			currentNumIndex = i;
+		while(noSwaps) {
+			// reset indexes
+			currentNumIndex = 0;
+			tempSeqNum = seqNum;
 			
-			// backtrack through sorted subset of array
-			for(int j = i-1; j >= 0; j--) {
-				// swap
-				if(currentNum < array[j]) {
-					array[currentNumIndex] = array[j];
-					array[j] = currentNum;
-					
-					// update currentNumIndex to the new location of currentNum
-					currentNumIndex = j;
-				}
+			// compare data at index with data at sequence # (h+index)
+			for(int i = 0; i < array.length; i++) {
+				numAtIndex = array[i];
 				
+				currentNumIndex = i;
+				
+				if(array[i] < array[i + seqNum]) {
+					
+					
+					noSwaps = false;
+				}
 			}
+			
+			//
+			
+			
+			
+			
+			// select next unsorted num in array and (re)set currentNumIndex
+			for (int i = 0; i < array.length; i++, h++) {
+				
+				// backtrack through sorted subset of array
+				for (int j = i - 1; j >= 0; j--) {
+					// swap
+					if (array[i] < array[j]) {
+						array[currentNumIndex] = array[j];
+						array[j] = currentNum;
+						
+						// update currentNumIndex to the new location of currentNum
+						currentNumIndex = j;
+					}
+					
+				}
+			}
+		
 		}
-	
+		
 	}
 	
 	public void recursive(IntegerArray array) {
