9af908babafdb4d23c29b37e12fbc11bd442d499
Ed <edoisa6@gmail.com>
2017-12-02 00:21:01 -0800

    quickSort progress

--- QuickSort.java
+++ QuickSort.java
@@ -8,20 +8,41 @@ public class QuickSort {
 		rand.setSeed(System.currentTimeMillis());
 		
 		// assign pivot
-		int pivot = array[rand.nextInt(array.length)];
+		int pivotIndex = rand.nextInt(array.length);
+		int pivot = array[pivotIndex];
 		
-		// sub-lists
-		int[] left = new int[array.length]; // sub-list left of pivot
-		int[] right = new int[array.length]; // sub-list right of pivot
+		// partition array
+		int[] smaller = new int[array.length];
+		int[] larger  = new int[array.length];
 		
-		for(int i = 0; i < array.length; i++) {
-		
+		for(int i = 0, current; i < array.length; i++) {
+			current = array[i];
+			
+			if(current <= pivot)
+				smaller[i] = current;
+			else
+				larger[i] = current;
 		}
+		
+		// copy smaller array onto main array
+		for(int i = 0; i < smaller.length; i++) {
+			array[i] = smaller[i];
+		}
+		
+		// copy pivot
+		array[smaller.length] = pivot;
+		pivotIndex = smaller.length; // reset pivotIndex
+		
+		// copy larger array onto main array after pivot
+		for(int i = 0; i < larger.length; i++) {
+			array[pivotIndex + i] = larger[i];
+		}
+		
 		
 		
 	}
 	
-	public void recursive(int[] array) {
+	public void recursive(int[] array, int low, int high) {
 	
 	}
 	
