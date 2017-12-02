3c3d2323ce7f1462683ceab3e5dc1fa812b5e268
Ed <edoisa6@gmail.com>
2017-12-01 21:44:33 -0800

    quickSort in progress

--- QuickSort.java
+++ QuickSort.java
@@ -1,10 +1,27 @@
+import java.util.Random;
+
 public class QuickSort {
 	
-	public void iterative(IntegerArray array) {
-	
+	public void iterative(int[] array) {
+		// generate random num for pivot
+		Random rand = new Random();
+		rand.setSeed(System.currentTimeMillis());
+		
+		// assign pivot
+		int pivot = array[rand.nextInt(array.length)];
+		
+		// sub-lists
+		int[] left = new int[array.length]; // sub-list left of pivot
+		int[] right = new int[]; // sub-list right of pivot
+		
+		for(int i = 0; i < array.length; i++) {
+		
+		}
+		
+		
 	}
 	
-	public void recursive(IntegerArray array) {
+	public void recursive(int[] array) {
 	
 	}
 	
